e8a552f861a8a1b58b23e3e882899b2312c99048
Proladon <proladon@gmail.com>
2021-07-12 20:04:59 +0800

    FileInput / add gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
-    <SZButton />
+    <div>
+      <p class="pl-2 text-xl">Button</p>
+      <SZButton />
+    </div>
+
+    <div>
+      <p class="pl-2 text-xl">Input</p>
+      <SZFileInputVue />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import SZButton from './components/button/SZButton.vue'
+import SZFileInputVue from './components/input/SZFileInput.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    SZButton
+    SZButton,
+    SZFileInputVue,
   }
 })
 </script>
 
 <style lang="postcss" scoped>
+  section {
+    @apply mb-5;
+  }
 </style>
---
+++ src/components/input/SZFileInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <label class="sz-file-input">
+    上傳圖片
+    <input class="input-hide" type="file" name="" id="" />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: "SZFileInput",
+})
+</script>
+
+<style scoped lang="postcss">
+
+.sz-file-input {
+  box-sizing: border-box;
+  @apply m-2  px-3 py-1 border border-solid border-teal-400 rounded-md;
+}
+.input-hide {
+  display: none;
+}
+</style>
